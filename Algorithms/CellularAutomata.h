67cfd0e9cd514d6ffe54c66f6e2e76eabe792d90
lashencodesthings <ldha0004@student.monash.edu>
2026-03-29 19:34:31 +1100

    Completed Cellular Automata 3rd Optimisation pass by using unsigned ints instead of bools to save space, and doing addition on them rather than conditions to preserve time spent

--- Algorithms/CellularAutomata.h
+++ Algorithms/CellularAutomata.h
@@ -2,92 +2,77 @@
 #include "splashkit.h"
 #include "../Data/WorldGenerationHeaders.h"
 #include <random>
+#include <vector>
 
 class CellularAutomata {
 public:
-    CellularAutomata(World &target_world) : world(target_world), rng(target_world.seed) {}
+    CellularAutomata(World& world_data) : world(world_data), rng(world_data.seed) {}
 
     void run() {
-        world.blocks = seed_initial_noise_pockets();
+        seed_noise();
         apply();
     }
 
 private:
-    World &world;
+    World& world;
     std::mt19937 rng;
 
     const int WALL_THRESHOLD = 4;
     const int NOISE_DENSITY = 35;
     const int ITERATIONS = 12;
-    
     const int OFFSET = 5;
 
-    int random_percent() {
-        std::uniform_int_distribution<int> dist(0, 99);
-        return dist(rng);
-    }
+    std::uniform_int_distribution<int> percent_dist{0, 99};
 
-    std::vector<std::vector<Block>> seed_initial_noise_pockets() {
-        std::vector<std::vector<Block>> noise_grid = world.blocks;
-        for (int x = 0; x < world.width; x++) {
-            for (int y = 0; y < world.height; y++) {
-                int random = random_percent();
-                if (y < world.get_surface_height(x) + OFFSET) continue;
+    inline bool add_air_hole() { return percent_dist(rng) < NOISE_DENSITY; }
 
-                if(random < NOISE_DENSITY) { 
-                    noise_grid[x][y] = Block(BlockType::Air, Solid, noise_grid[x][y].wall); 
-                }
-                else
-                {
-                    noise_grid[x][y] = Block(noise_grid[x][y].wall);
-                }
-            }
-        }
-        return noise_grid;
-    }
+    void seed_noise() {
+        for (int x = 0; x < world.width; ++x) {
+            int surface_height = world.get_surface_height(x) + OFFSET;
 
-    void apply()
-    {
-        int width = world.width;
-        int height = world.height;
-
-        std::vector<bool> current_bits(width * height);
-        std::vector<bool> iteration_bits(width * height);
-        std::vector<int> surface_cache(width);
-
-        for (int x = 0; x < width; x++) {
-            surface_cache[x] = world.get_surface_height(x) + OFFSET;
-            for (int y = 0; y < height; y++) {
-                current_bits[x + y * width] = (world.blocks[x][y].type != BlockType::Air);
-            }
-        }
-
-        for (int iteration = 0; iteration < ITERATIONS; iteration++) {
-            for (int y = 0; y < height; y++) {
-                for (int x = 0; x < width; x++) {
-                    int neighbors = 0;
-                    for (int ny = y - 1; ny <= y + 1; ny++) {
-                        for (int nx = x - 1; nx <= x + 1; nx++) {
-                            if (nx == x && ny == y) continue;
-                            if (nx < 0 || nx >= width || ny < 0 || ny >= height) {
-                                neighbors++;
-                            } else if (current_bits[nx + ny * width]) {
-                                neighbors++;
-                            }
-                        }
-                    }
-                    iteration_bits[x + y * width] = (neighbors > WALL_THRESHOLD);
-                }
-            }
-            current_bits.swap(iteration_bits); // Swap pointers rather than reassigning to use less memory
-        }
-
-        for (int x = 0; x < width; x++) {
-            for (int y = surface_cache[x]; y < height; y++) {
-                if (current_bits[x + y * width])
-                    world.blocks[x][y] = Block(world.blocks[x][y].wall);
-                else
+            for (int y = surface_height; y < world.height; ++y) {
+                if (add_air_hole())
                     world.blocks[x][y] = Block(BlockType::Air, Solid, world.blocks[x][y].wall);
+                else
+                    world.blocks[x][y] = Block(world.blocks[x][y].wall);
+            }
+        }
+    }
+
+    void apply() {
+        const int width = world.width;
+        const int height = world.height;
+
+        std::vector<uint8_t> current(width * height);
+        std::vector<uint8_t> next(width * height);
+        std::vector<int> surface_height(width);
+
+        for (int x = 0; x < width; ++x) {
+            surface_height[x] = world.get_surface_height(x) + OFFSET;
+            for (int y = 0; y < height; ++y) { current[x + y * width] = (world.blocks[x][y].type != BlockType::Air); }
+        }
+
+        for (int iteration = 0; iteration < ITERATIONS; ++iteration) {
+            next = current;
+            for (int y = 1; y < height - 1; ++y) {
+                int row = y * width;
+
+                for (int x = 1; x < width - 1; ++x) {
+                    int block_index = x + row;
+                    int neighbours =
+                        current[block_index - width - 1] + current[block_index - width] + current[block_index - width + 1] +
+                        current[block_index - 1] + current[block_index + 1] +
+                        current[block_index + width - 1] + current[block_index + width] + current[block_index + width + 1];
+
+                    next[block_index] = (neighbours >= WALL_THRESHOLD);
+                }
+            }
+        }
+
+        for (int x = 0; x < width; ++x) {
+            for (int y = surface_height[x]; y < height; ++y) {
+                int block_index = x + y * width;
+                world.blocks[x][y] = current[block_index] ? Block(world.blocks[x][y].wall) : Block(BlockType::Air, Solid, world.blocks[x][y].wall);
             }
         }
     }
