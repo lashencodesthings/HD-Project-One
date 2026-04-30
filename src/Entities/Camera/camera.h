c5828b4e1ecdc8778a6fd2f880912c5589502f00
lashencodesthings <ldha0004@student.monash.edu>
2026-04-30 10:20:05 +1000

    Added a meter

--- src/Entities/Camera/camera.h
+++ src/Entities/Camera/camera.h
@@ -1,8 +1,7 @@
 #pragma once
-struct Camera
-{
-    double x, y;
-    double smooth_factor = 0.1f;
-};
+// Forward declaration to tell the compiler the Player is a struct that exists
+
+struct Player; 
+struct Camera;
 
 void update_camera(Camera &camera, Player &player);
